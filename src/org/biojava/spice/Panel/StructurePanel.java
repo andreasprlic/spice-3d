13e422030348a6abf7c62a9af6c1923f0be93759
pandreas <andreas.prlic@gmail.com>
2004-10-22 10:05:38 +0000

    improved docu

--- src/org/biojava/spice/Panel/StructurePanel.java
+++ src/org/biojava/spice/Panel/StructurePanel.java
@@ -1,3 +1,25 @@
+/*
+ *                    BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ *
+ *
+ */
+
 package org.biojava.spice ;
 
 // for Jmol stuff
@@ -10,13 +32,17 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-// structure stuff
+// biojava structure stuff
 import org.biojava.bio.structure.Structure ;
-//import org.biojava.bio.structure.io.PDBFileReader ;
 
 
 
-/** a Panel that provides a wrapper around the Jmol viewer */
+/** a Panel that provides a wrapper around the Jmol viewer. Code heavily
+ * inspired by
+ * http://cvs.sourceforge.net/viewcvs.py/jmol/Jmol/examples/Integration.java?view=markup
+ * - the Jmol example of how to integrate Jmol into an application.
+ * 
+ */
 class StructurePanel extends JPanel {
 
     JmolViewer viewer;
@@ -52,7 +78,8 @@ class StructurePanel extends JPanel {
     }
 
     /** display a new PDB structure in Jmol 
-     * @param structre a Biojava structure object     
+     * @param structure a Biojava structure object    
+     *
      */
     public  void setStructure(Structure structure) {
 	
