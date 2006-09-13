cdff91b49258c945c9937a317fc8afc7e9d1ac35
pandreas <andreas.prlic@gmail.com>
2006-09-13 13:17:59 +0000

    a new class that will take care of some defaults

---
+++ src/org/biojava/spice/config/SpiceDefaults.java
@@ -0,0 +1,36 @@
+/*
+ *                  BioJava development code
+ *
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public Licence.  This should
+ * be distributed with the code.  If you do not have a copy,
+ * see:
+ *
+ *      http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright for this code is held jointly by the individual
+ * authors.  These should be listed in @author doc comments.
+ *
+ * For more information on the BioJava project and its aims,
+ * or to join the biojava-l mailing list, visit the home page
+ * at:
+ *
+ *      http://www.biojava.org/
+ * 
+ * Created on Sep 13, 2006
+ * 
+ */
+
+package org.biojava.spice.config;
+
+import java.awt.Color;
+
+public class SpiceDefaults {
+
+
+    public static final Color HELIX_COLOR  = new Color(255,51,51);
+    public static final Color STRAND_COLOR = new Color(255,204,51);
+    public static final Color TURN_COLOR   = new Color(204,204,204); 
+    
+	
+}
