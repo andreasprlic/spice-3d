c4038146836d937711411bc78986486e1e6ea7d6
pandreas <andreas.prlic@gmail.com>
2006-09-15 09:30:27 +0000

    small cleanup in spice startup types

--- src/org/biojava/spice/config/SpiceDefaults.java
+++ src/org/biojava/spice/config/SpiceDefaults.java
@@ -24,6 +24,8 @@
 package org.biojava.spice.config;
 
 import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 import org.biojava.spice.ResourceManager;
@@ -35,10 +37,21 @@ public class SpiceDefaults {
 	// logging stuff
 	public static final String LOGGER = "org.biojava.spice";
 	public static final Level  LOG_LEVEL = Level.INFO;
-
+	
+	public static final String REGISTRY    = ResourceManager.getString("org.biojava.spice.SpiceDefaults.Registry");
+	
     public static final Color HELIX_COLOR  = new Color(255,51,51);
     public static final Color STRAND_COLOR = new Color(255,204,51);
     public static final Color TURN_COLOR   = new Color(204,204,204); 
-    public static final String REGISTRY    = ResourceManager.getString("org.biojava.spice.SpiceDefaults.Registry");
-	
+    
+    public static final List argumentTypes;
+    
+    	static { 
+    			argumentTypes = new ArrayList();
+    			argumentTypes.add("PDB");
+    			argumentTypes.add("UniProt");
+    			argumentTypes.add("ENSP");
+    			argumentTypes.add("alignment");
+    	}
+    
 }
--- src/org/biojava/spice/gui/OpenDialog.java
+++ src/org/biojava/spice/gui/OpenDialog.java
@@ -39,6 +39,7 @@ import javax.swing.JTextField;
 import org.biojava.spice.ResourceManager;
 import org.biojava.spice.SPICEFrame;
 import org.biojava.spice.SpiceApplication;
+import org.biojava.spice.config.SpiceDefaults;
 import org.biojava.spice.gui.msdkeyword.*;
 
 import javax.swing.JLabel;
@@ -53,194 +54,188 @@ import javax.swing.JLabel;
 public class OpenDialog 
 extends JDialog 
 {
-    
-    private static final long serialVersionUID = 2832023723402743924L;
-    
-    
-    static final String[] supportedCoords = { "PDB","UniProt","ENSP", "alignment"};
-    //static final String[] supportedCoords = { "PDB","UniProt"};
-    static int H_SIZE = 350;
-    static int V_SIZE = 150 ;
-    SpiceApplication spice       ;
-    JTextField getCom      ;
-    JComboBox  list        ;
-    String     currentType ;
-    JTextField kwsearch;     
-    
-    
-    /** a dialog responsible for opening new entries.
-     * 
-     * @param parent the parent SpiceApplication instance
-     */
-    public OpenDialog(SpiceApplication parent){
-        // Calls the parent telling it this
-        // dialog is modal(i.e true)
-        
-        
-        this.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent evt) {
-                JFrame frame = (JFrame) evt.getSource();
-                frame.setVisible(false);
-                frame.dispose();
-            }
-        });
-        
-        spice = parent ;
-        
-        this.setTitle("enter code") ;
-        
-        JPanel p = new JPanel();
-        
-        Box vBox  = Box.createVerticalBox();
-        Box hBox1 = Box.createHorizontalBox();
-        Box hBox2 = Box.createHorizontalBox();
-        Box hBox3 = Box.createHorizontalBox();
-        
-        int startpos   = 0;
-        currentType    = supportedCoords[startpos];
-        
-        list = new JComboBox(supportedCoords) ;		
-        list.setEditable(false);
-        list.setMaximumSize(new Dimension(Short.MAX_VALUE,30));
-        list.setSelectedIndex(startpos);
-        
-        list.addActionListener(new ActionListener() {
-        
-            public void actionPerformed(ActionEvent arg0) {
-                //System.out.println("action performed");
-                setHelpToolTip();     
-        
-            }        
-        });
-        
-        
-        
-        hBox1.add(list);
-        
-        
-        getCom = new JTextField(10);
-        //TextFieldListener txtlisten = new TextFieldListener(parent,getCom);
-        //getCom.addActionListener(txtlisten);
-        
-        getCom.addActionListener(new ActionListener()  {
-            public void actionPerformed(ActionEvent e) {
-                
-                String code = getCom.getText();
-                String type = (String)list.getSelectedItem() ;
 
-                
-                if (    type.equals("PDB") ||
-                        type.equals("ENSP") ||
-                        type.equals("UniProt") ||
-                        type.equals("alignment")){
-                    spice.load(type,code);
-                    dispose();			    
-                } else {
-                    System.out.println("unknown type " + type + " code " + code);
-                }
-            }
-        });
-        
-      
-        
-        hBox1.add(getCom);
-       
-        
-        JButton openB =new JButton(ResourceManager.getString("org.biojava.spice.action.open"));
-        openB.addActionListener(new ButtonListener(spice,this));
-        
-        JButton cancelB = new JButton(ResourceManager.getString("org.biojava.spice.action.cancel"));
-        cancelB.addActionListener(new ButtonListener(spice,this));
-        
-        hBox1.add(openB);
-        hBox3.add(cancelB);
-        setHelpToolTip();
-        
-        kwsearch = new JTextField(10);
-        kwsearch.addActionListener(new ActionListener()  {
-            public void actionPerformed(ActionEvent e) {
-                String kw  = kwsearch.getText();            
-                new MSDWindow(spice,kw);              
-                dispose();			    
-            }
-            
-        });
-        
-        JButton openKw = new JButton(ResourceManager.getString("org.biojava.spice.gui.ConfigPanel.Search"));
-        openKw.addActionListener(new ButtonListener(spice,this));
-        JLabel kwl = new JLabel("keyword:");
-        hBox2.add(kwl);
-        hBox2.add(kwsearch);
-        hBox2.add(openKw);
-        
-        
-        vBox.add(hBox1);
-        vBox.add(hBox2);
-        vBox.add(hBox3);
-        
-        
-        p.add(vBox);
-        this.getContentPane().add(p);
-        
-        this.setSize(H_SIZE, V_SIZE);
-    }
-    
-    private void setHelpToolTip(){
-        String type = (String) list.getSelectedItem();
-        if ( type.equals("PDB")){
-            getCom.setToolTipText("enter PDB code or PDB code + chain. e.g. 1tim.B");
-        } else if ( type.equals("UniProt")){
-            getCom.setToolTipText("enter UniProt accession code e.g. P50225");            
-        } else if ( type.equals("ENSP")){
-            getCom.setToolTipText("enter Ensembl peptide accession code e.g. ENSP00000334713");
-        }
-        // todo: alignment helper
-        
-            
-    }
-    
+	private static final long serialVersionUID = 2832023723402743924L;
+
+
+	static final String[] supportedCoords = { "PDB","UniProt","ENSP", "alignment"};
+	//static final String[] supportedCoords = { "PDB","UniProt"};
+	static int H_SIZE = 350;
+	static int V_SIZE = 150 ;
+	SpiceApplication spice       ;
+	JTextField getCom      ;
+	JComboBox  list        ;
+	String     currentType ;
+	JTextField kwsearch;     
+
+
+	/** a dialog responsible for opening new entries.
+	 * 
+	 * @param parent the parent SpiceApplication instance
+	 */
+	public OpenDialog(SpiceApplication parent){
+		// Calls the parent telling it this
+		// dialog is modal(i.e true)
+
+
+		this.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent evt) {
+				JFrame frame = (JFrame) evt.getSource();
+				frame.setVisible(false);
+				frame.dispose();
+			}
+		});
+
+		spice = parent ;
+
+		this.setTitle("enter code") ;
+
+		JPanel p = new JPanel();
+
+		Box vBox  = Box.createVerticalBox();
+		Box hBox1 = Box.createHorizontalBox();
+		Box hBox2 = Box.createHorizontalBox();
+		Box hBox3 = Box.createHorizontalBox();
+
+		int startpos   = 0;
+		currentType    = supportedCoords[startpos];
+
+		list = new JComboBox(supportedCoords) ;		
+		list.setEditable(false);
+		list.setMaximumSize(new Dimension(Short.MAX_VALUE,30));
+		list.setSelectedIndex(startpos);
+
+		list.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				//System.out.println("action performed");
+				setHelpToolTip();     
+
+			}        
+		});
+
+
+
+		hBox1.add(list);
+
+
+		getCom = new JTextField(10);
+
+
+		getCom.addActionListener(new ActionListener()  {
+
+			public void actionPerformed(ActionEvent e) {
+
+				String code = getCom.getText();
+				String type = (String)list.getSelectedItem() ;
+
+
+				if ( SpiceDefaults.argumentTypes.contains(type) ) {
+					spice.load(type,code);
+					dispose();			    
+				} else {
+					System.out.println("unknown type " + type + " code " + code);
+				}
+			}
+		});
+
+
+
+		hBox1.add(getCom);
+
+
+		JButton openB =new JButton(ResourceManager.getString("org.biojava.spice.action.open"));
+		openB.addActionListener(new openActionListener(spice,this));
+
+		JButton cancelB = new JButton(ResourceManager.getString("org.biojava.spice.action.cancel"));
+		cancelB.addActionListener(new openActionListener(spice,this));
+
+		hBox1.add(openB);
+		hBox3.add(cancelB);
+		setHelpToolTip();
+
+		kwsearch = new JTextField(10);
+		kwsearch.addActionListener(new ActionListener()  {
+			public void actionPerformed(ActionEvent e) {
+				String kw  = kwsearch.getText();            
+				new MSDWindow(spice,kw);              
+				dispose();			    
+			}
+
+		});
+
+		JButton openKw = new JButton(ResourceManager.getString("org.biojava.spice.gui.ConfigPanel.Search"));
+		openKw.addActionListener(new openActionListener(spice,this));
+		JLabel kwl = new JLabel("keyword:");
+		hBox2.add(kwl);
+		hBox2.add(kwsearch);
+		hBox2.add(openKw);
+
+
+		vBox.add(hBox1);
+		vBox.add(hBox2);
+		vBox.add(hBox3);
+
+
+		p.add(vBox);
+		this.getContentPane().add(p);
+
+		this.setSize(H_SIZE, V_SIZE);
+	}
+
+	private void setHelpToolTip(){
+		String type = (String) list.getSelectedItem();
+		if ( type.equals("PDB")){
+			getCom.setToolTipText("enter PDB code or PDB code + chain. e.g. 1tim.B");
+		} else if ( type.equals("UniProt")){
+			getCom.setToolTipText("enter UniProt accession code e.g. P50225");            
+		} else if ( type.equals("ENSP")){
+			getCom.setToolTipText("enter Ensembl peptide accession code e.g. ENSP00000334713");
+		}
+		// todo: alignment helper
+
+
+	}
+
 }
 
-class ButtonListener
-implements ActionListener{
-    SPICEFrame spice;
-    OpenDialog parent ;
-    
-    public ButtonListener( SPICEFrame spice_, OpenDialog parent_) {
-        spice = spice_ ;
-        parent = parent_ ;
-    }
-    
-    /* (non-Javadoc)
-     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
-    public void actionPerformed(ActionEvent e) {
-        
-        String cmd = e.getActionCommand();
-        if(cmd.equals("Cancel"))
-        {
-            parent.dispose();
-            return ;
-        }
-        else if ( cmd.equals("Open"))
-        { 
-            String type = (String)parent.list.getSelectedItem() ;
-            String code = parent.getCom.getText();
-          
-            if ( type.equals("PDB")  ||
-                    type.equals("ENSP") ||
-                    type.equals("UniProt") ||
-                    type.equals("alignment")){
-                spice.load(type,code);
-                parent.dispose();			    
-            }
-            return ;	
-        }
-        else if ( cmd.equals("Search")){
-            String kw  = parent.kwsearch.getText();
-	             
-            new MSDWindow(spice,kw);    
-            parent.dispose();
-        }  
-    } 
+class openActionListener implements ActionListener {
+	
+	OpenDialog parent;
+	SPICEFrame spice;
+
+	public openActionListener(SPICEFrame spice, OpenDialog parent){
+		this.spice = spice;
+		this.parent = parent ;
+	}
+
+	public void actionPerformed(ActionEvent e) {
+
+		String cmd = e.getActionCommand();
+		if(cmd.equals("Cancel"))
+		{
+			parent.dispose();
+			return ;
+		}
+		else if ( cmd.equals("Open"))
+		{ 
+			String type = (String)parent.list.getSelectedItem() ;
+			String code = parent.getCom.getText();
+
+			if ( SpiceDefaults.argumentTypes.contains(type)){
+				spice.load(type,code);
+				parent.dispose();			    
+			}
+			return ;	
+		}
+		else if ( cmd.equals("Search")){
+			String kw  = parent.kwsearch.getText();
+
+			new MSDWindow(spice,kw);    
+			parent.dispose();
+		}  
+	} 
+
+
 }
+
